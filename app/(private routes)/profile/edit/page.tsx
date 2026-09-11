1c6cc447b252cd23e8fbb3e90622517a602f971b
kondratenko-go-dev <kondratenko.aleksandr.go.dev@gmail.com>
2026-09-11 08:48:29 +0300

    Add profile and edit profile pages

--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -1,5 +1,89 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
+import { updateMe } from '@/lib/api/clientApi';
+import { useAuthStore } from '@/lib/store/authStore';
+
+import css from './EditProfilePage.module.css';
+
 const EditProfile = () => {
-  return <main>Edit profile</main>;
+  const router = useRouter();
+
+  const user = useAuthStore(state => state.user);
+  const setUser = useAuthStore(state => state.setUser);
+
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      setError('');
+
+      const username = formData.get('username') as string;
+      const updatedUser = await updateMe({ username });
+
+      setUser(updatedUser);
+      router.push('/profile');
+    } catch {
+      setError('Failed to update profile. Please try again.');
+    }
+  };
+
+  const handleCancel = () => {
+    router.push('/profile');
+  };
+
+  return (
+    <main className={css.mainContent}>
+      <div className={css.profileCard}>
+        <h1 className={css.formTitle}>Edit Profile</h1>
+
+        {user && (
+          <Image
+            src={user.avatar}
+            alt="User Avatar"
+            width={120}
+            height={120}
+            className={css.avatar}
+          />
+        )}
+
+        <form className={css.profileInfo} action={handleSubmit}>
+          <div className={css.usernameWrapper}>
+            <label htmlFor="username">Username:</label>
+            <input
+              id="username"
+              name="username"
+              type="text"
+              className={css.input}
+              defaultValue={user?.username}
+              required
+            />
+          </div>
+
+          <p>Email: {user?.email}</p>
+
+          <div className={css.actions}>
+            <button type="submit" className={css.saveButton}>
+              Save
+            </button>
+
+            <button
+              type="button"
+              className={css.cancelButton}
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+
+        {error && <p className={css.error}>{error}</p>}
+      </div>
+    </main>
+  );
 };
 
 export default EditProfile;
